718b97744d42068d4054a216a072c95e9c73a3e1
igor cunha <igorcunha.melo1@gmail.com>
2023-09-01 10:57:00 -0300

    modificar idioma

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Botão adicionar',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,7 +28,8 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 58, 183, 141)),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'CONTADOR'),
@@ -38,16 +39,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -59,11 +50,6 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
@@ -80,11 +66,11 @@ class _MyHomePageState extends State<MyHomePage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               const Text(
-                'You have pushed the button this many times:',
+                'Você apertou:',
               ),
               Text(
-                '$_counter',
-                style: Theme.of(context).textTheme.headlineMedium,
+                '$_counter vezes',
+                style: Theme.of(context).textTheme.headlineLarge,
               ),
             ],
           ),
@@ -92,7 +78,7 @@ class _MyHomePageState extends State<MyHomePage> {
         floatingActionButton: FloatingActionButton.extended(
             onPressed: _incrementCounter,
             tooltip: 'Increment',
-            label: const Text('Add'),
+            label: const Text('adicionar'),
             icon: const Icon(Icons.add)
             // This trailing comma makes auto-formatting nicer for build methods.
             ));
